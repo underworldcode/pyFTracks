ff86279838d8eeffa73af698d5d03c00019cadab
Romain Beucher <rbeucher@thermo-geology.fr>
2019-08-30 08:20:31 +1000

    Use pyRadialPlot as a separate package

--- pyFTracks/__init__.py
+++ pyFTracks/__init__.py
@@ -2,5 +2,6 @@ from .annealing import Ketcham1999, Ketcham2007
 from .sample import Sample
 from .grain import Grain
 from .viewer import Viewer
-from .radialplot import radialplot
+from pyRadialPlot import radialplot
 from .age_calculations import *
+
