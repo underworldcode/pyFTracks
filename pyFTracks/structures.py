0bbba9db935ccb0658be69f8c1d08324c1a9254e
Romain Beucher <rbeucher@thermo-geology.fr>
2019-11-12 23:39:34 +0000

    Change structures, merge into one file

--- pyFTracks/structures.py
+++ pyFTracks/structures.py
@@ -2,32 +2,27 @@ from itertools import count
 import numpy as np
 from collections import OrderedDict
 from pandas import Series, DataFrame
+from pyRadialPlot import radialplot
 import pandas as pd
 from .utilities import read_mtx_file, h5load, h5store
+from .age_calculations import calculate_ages
+from .age_calculations import calculate_pooled_age
+from .age_calculations import calculate_central_age
+
+unprojected_coefs = {"ETCH_PIT_LENGTH": {"m": 0.283, "b": 15.63},
+               "CL_PFU": {"m": 0.544, "b": 16.18},
+               "OH_PFU": {"m": 0.0, "b": 16.18},
+               "CL_WT_PCT": {"m": 0.13824, "b": 16.288}}
+
+projected_coefs = {"ETCH_PIT_LENGTH": {"m": 0.205, "b": 16.10},
+             "CL_PFU": {"m": 0.407, "b": 16.49},
+             "OH_PFU": {"m": 0.000, "b": 16.57},
+             "CL_WT_PCT": {"m": 0.17317, "b": 16.495}}
+
 
 class Grain(Series):
 
-    # The Following parameters are taken from Carlson et al 1999
-    # Equations 1..4
-    unprojected = {"ETCH_PIT_LENGTH": {"m": 0.283, "b": 15.63},
-                   "CL_PFU": {"m": 0.544, "b": 16.18},
-                   "OH_PFU": {"m": 0.0, "b": 16.18},
-                   "CL_WT_PCT": {"m": 0.13824, "b": 16.288}}
-
-    projected = {"ETCH_PIT_LENGTH": {"m": 0.205, "b": 16.10},
-                 "CL_PFU": {"m": 0.407, "b": 16.49},
-                 "OH_PFU": {"m": 0.000, "b": 16.57},
-                 "CL_WT_PCT": {"m": 0.17317, "b": 16.495}}
-
-    _ids = count(0)
-    
-    _added_properties = ['id', 'l0_user_defined', 'l0', '_l0']
-    _internal_names = Series._internal_names + _added_properties
-    _internal_names_set = set(_internal_names)
-    
     def __init__(self, *args, **kwargs):
-
-        self.id = next(self._ids)
         Series.__init__(self, *args, **kwargs)
 
     @property
@@ -38,52 +33,16 @@ class Grain(Series):
     def _constructor_expanddim(self):
         return Sample
 
-    @property
-    def l0(self):
-        if not self.l0_user_defined:
-            m = Grain.unprojected[self.kinetic_parameter_type]["m"]
-            b = Grain.unprojected[self.kinetic_parameter_type]["b"]
-            self._l0 = m * self.kinetic_parameter_value + b
-        return self._l0
-
-    @l0.setter
-    def l0(self, length):
-        if length:
-            self.l0_user_defined = True
-            self._l0 = length
-        else:
-            self._l0 = None
-#
-#    @property
-#    def l0_projected(self):
-#        if not self.l0_projected_user_defined:
-#            m = Grain.projected[self.kinetic_parameter_type]["m"]
-#            b = Grain.projected[self.kinetic_parameter_type]["b"]
-#            self._l0_projected = m * self.kinetic_parameter_value + b
-#        return self._l0_projected
-#    
-#    @l0_projected.setter
-#    def l0_projected(self, length):
-#        if length:
-#            self.l0_projected_user_defined = True
-#            self._l0_projected = length
-#        else:
-#            self._l0_projected = None
     
-Population = Apatite = Crystal = Grain
-
-
 class Sample(DataFrame):
 
-    _ids = count(0)
-
     # Let pandas know what properties are added
-    _added_properties = ['name', 'zeta', 'zeta_error', 
+    _metadata = ['name', 'zeta', 'zeta_error', 
+            'pooled_age', 'pooled_age_se', 'central_age',
+            'central_age_se', 'central_age_se',
             'rhod', 'nd', 'depth', 'elevation',
             'stratigraphic_age', 'stratigraphic_age_name',
             'deposition_temperature', 'present_day_temperature', 'id']
-    _internal_names = DataFrame._internal_names + _added_properties
-    _internal_names_set = set(_internal_names)
 
     @property
     def _constructor(self):
@@ -93,52 +52,59 @@ class Sample(DataFrame):
     def _constructor_sliced(self):
         return Grain
 
-    def __init__(self,
-                 file=None,
-                 name=None, 
-                 zeta=None,
-                 zeta_error=None,
-                 rhod=None,
-                 nd=None,
-                 depth=None, 
-                 elevation=None,
-                 stratigraphic_age=None, 
-                 stratigraphic_age_name=None, 
-                 deposition_temperature=None,
-                 present_day_temperature=None,
-                 grains=None,
-                 counts=None,
-                 Ns=None,
-                 Ni=None, A=None, track_lengths=None, l0=None,
-                 Dpars=None, Cl=None, projected=True,
-                 description=None
-                 ):
+    def read_from_hdf5(self, filename):
+        with pd.HDFStore(filename) as store:
+            data, metadata = h5load(store)
+        for val in self._metadata:
+            try:
+                key = val.replace("_", " ")
+                setattr(self, val, metadata.pop(key))
+            except:
+                pass
+        self.__init__(data)
 
-        self.id = next(self._ids)
 
-        if file:
-            with pd.HDFStore(file) as store:
-                data, metadata = h5load(store)
-            for val in self._added_properties:
-                try:
-                    key = val.replace("_", " ")
-                    setattr(self, val, metadata.pop(key))
-                except:
-                    pass
-            DataFrame.__init__(self, data)
+    def read_from_radialplotter(self, filename):
+        from pyRadialPlot import read_radialplotter_file
+        data = read_radialplotter_file(filename)
+         
+        self.__init__({"Ns": data["Ns"], "Ni": data["Ni"]})
+        self.zeta = data["zeta"]
+        self.rhod = data["rhod"]
+    
+    def calculate_l0_from_Dpars(self, projected=True):
+        if projected:
+            m = projected_coefs["ETCH_PIT_LENGTH"]["m"]
+            b = projected_coefs["ETCH_PIT_LENGTH"]["b"]
         else:
-            self.name = name
-            self.zeta = zeta
-            self.zeta_error = zeta_error
-            self.rhod = rhod
-            self.nd = nd
-            self.depth = depth
-            self.elevation = elevation
-            self.stratigraphic_age = stratigraphic_age
-            self.stratigraphic_age_name = stratigraphic_age_name
-            self.deposition_temperature = deposition_temperature
-            self.present_day_temperature = present_day_temperature
-            DataFrame.__init__(self)
+            m = unprojected_coefs["ETCH_PIT_LENGTH"]["m"]
+            b = unprojected_coefs["ETCH_PIT_LENGTH"]["b"]
+        if not hasattr(self, "Dpars"):
+            raise ValueError("Cannot find Dpars column")
+        self["l0"] = m * self.Dpars + b
+
+    def calculate_ages(self):
+        data =  calculate_ages(
+                self.Ns, self.Ni, self.zeta, 
+                self.zeta_error, self.rhod, self.nd)
+        self["Ages"] = data["Age(s)"]
+        self["Ages Errors"] = data["se(s)"]
+
+    def calculate_pooled_age(self):
+        data = calculate_pooled_age(
+                self.Ns, self.Ni, self.zeta,
+                self.zeta_error, self.rhod, self.nd)
+        self.pooled_age = data["Pooled Age"]
+        self.pooled_age_se = data["se"]
+
+    def calculate_central_age(self):
+        data = calculate_central_age(
+                self.Ns, self.Ni, self.zeta,
+                self.zeta_error, self.rhod, self.nd
+                )
+        self.central_age = data["Central"]
+        self.central_age_se = data["se"]
+        self.central_age_sigma = data["sigma"]
 
     def _repr_html_(self):
         """_repr_html_
@@ -147,15 +113,15 @@ class Sample(DataFrame):
         For integration with Jupyter notebook.
         """
         params = OrderedDict()
-        params["Name"] = self.name
-        params["Depth"] = self.depth
-        params["Elevation"] = self.elevation
-        params["Stratigraphic Age Range Upper/Lower"] = self.stratigraphic_age
-        params["Stratigraphic Age Name"] = self.stratigraphic_age_name
-        params["Deposition Temperature"] = self.deposition_temperature
-        params["Present Day Temperature"] = self.present_day_temperature
-        params["Total Ns"] = sum(self.Ns)
-        params["Total Ni"] = sum(self.Ni)
+        #params["Name"] = self.name
+        #params["Depth"] = self.depth
+        #params["Elevation"] = self.elevation
+        #params["Stratigraphic Age Range Upper/Lower"] = self.stratigraphic_age
+        #params["Stratigraphic Age Name"] = self.stratigraphic_age_name
+        #params["Deposition Temperature"] = self.deposition_temperature
+        #params["Present Day Temperature"] = self.present_day_temperature
+        #params["Total Ns"] = sum(self.Ns)
+        #params["Total Ni"] = sum(self.Ni)
     
         html = ""
 
@@ -167,6 +133,10 @@ class Sample(DataFrame):
 
     def save(self, filename):
         h5store(filename, *self.data_info)
+
+    def radialplot(self, transform="Logarithmic"):
+        return radialplot(self.Ns, self.Ni, self.zeta, self.rhod, transform=transform)
+
     
 
 
