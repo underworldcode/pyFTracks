97acffcc867d81c697fe6506fc6746621bdf6a26
Romain Beucher <rbeucher@thermo-geology.fr>
2019-11-13 00:03:06 +0000

    Import Miller and Gleadow samples / examples

--- pyFTracks/ressources/__init__.py
+++ pyFTracks/ressources/__init__.py
@@ -1,5 +1,10 @@
 import pandas as pd
 from pathlib import Path
 
-Miller1995 = pd.read_hdf((Path(__file__).parent / "Miller1995.h5"), "data")
-Gleadow = pd.read_hdf((Path(__file__).parent / "Gleadow.h5"), "data")
+#Miller1995 = pd.read_hdf((Path(__file__).parent / "Miller1995.h5"), "data")
+#Gleadow = pd.read_hdf((Path(__file__).parent / "Gleadow.h5"), "data")
+
+from pyFTracks import Sample
+
+Miller1995 = Sample().read_from_hdf5(Path(__file__).parent / "Miller.h5")
+Gleadow = Sample().read_from_hdf5(Path(__file__).parent / "Gleadow.h5")
