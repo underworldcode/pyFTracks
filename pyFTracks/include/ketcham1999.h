b7708656310d8f2cba5a2439d347249579bd01f6
Romain Beucher <rbeucher@thermo-geology.fr>
2019-11-13 00:04:48 +0000

    Add etchant parameter

--- pyFTracks/include/ketcham1999.h
+++ pyFTracks/include/ketcham1999.h
@@ -1,4 +1,4 @@
 
 void ketch99_reduced_lengths(double *time, double *temperature, int numTTNodes,
                              double *redLength,  double kinPar, int kinParType,
-                             int *firstTTNode);
+                             int *firstTTNode, int etchant);
