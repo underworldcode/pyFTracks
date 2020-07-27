c780b32fcfb174e338776d6c495aa024a2c0925d
Romain Beucher <romain.beucher@anu.edu.au>
2020-07-27 11:24:32 +1000

    cleaning

--- pyFTracks/__init__.py
+++ pyFTracks/__init__.py
@@ -1,8 +1,8 @@
-from .thermal_history import ThermalHistory
-from .annealing_model import AnnealingModel
+from .annealing_model import Ketcham1999, Ketcham2007
 from .structures import Grain, Sample
+from .viewer import Viewer
 from .age_calculations import calculate_central_age as central_age
 from .age_calculations import calculate_pooled_age as pooled_age
 from .age_calculations import calculate_ages as single_grain_ages
 from .age_calculations import chi_square as chi2_test
-from .viewer import Viewer
+from .thermal_history import ThermalHistory
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -2,6 +2,62 @@ import pyFTracks as FT
 import pytest
 import numpy as np
 
+
+def test_thermal_history():
+    thermal_history = FT.ThermalHistory(name="My Thermal History",
+        time=[0., 43., 44., 100.],
+        temperature=[283., 283., 403., 403.])
+    assert(np.all(thermal_history.input_time == np.array([0.0, 43.0, 44.0, 100.0])))
+    assert(np.all(thermal_history.input_temperature == np.array([283.0, 283.0, 403.0, 403.0])))
+
+def test_get_isothermal_interval():
+    thermal_history = FT.ThermalHistory(name="My Thermal History",
+        time=[0., 43., 44., 100.],
+        temperature=[283., 283., 403., 403.])
+    assert(np.all(np.isclose(thermal_history.time, np.array([
+        100., 99., 98., 97.,
+        96., 95., 94., 93.,
+        92., 91., 90., 89.,
+        88., 87., 86., 85.,
+        84., 83., 82., 81.,
+        80., 79., 78., 77.,
+        76., 75., 74., 73.,
+        72., 71., 70., 69.,
+        68., 67., 66., 65.,
+        64., 63., 62., 61.,
+        60., 59., 58., 57.,
+        56., 55., 54., 53.,
+        52., 51., 50., 49.,
+        48., 47., 46., 45.,
+        44., 43.93333333, 43.86666667, 43.8,
+        43.73333333,  43.66666667, 43.6,  43.53333333,
+        43.46666667,  43.4, 43.33333333, 43.26666667,
+        43.2,  43.13333333, 43.06666667, 43.,
+        42., 41., 40., 39.,
+        38., 37., 36., 35.,
+        34., 33., 32., 31.,
+        30., 29., 28., 27.,
+        26., 25., 24., 23.,
+        22., 21., 20., 19.,
+        18., 17., 16., 15.,
+        14., 13., 12., 11.,
+        10.,  9.,  8.,  7.,
+         6.,  5.,  4.,  3.,
+         2.,  1.,  0.]))))
+
+def test_isothermal_max_temperature_wolfs():
+    from pyFTracks.thermal_history import WOLF1, WOLF2, WOLF3, WOLF4, WOLF5
+    thermal_histories = [WOLF1, WOLF2, WOLF3, WOLF4, WOLF5]
+    for thermal_history in thermal_histories:
+        assert(np.diff(thermal_history.temperature).max() <= 8.0)
+
+def test_isothermal_max_temperature_wolfs2():
+    from pyFTracks.thermal_history import WOLF1, WOLF2, WOLF3, WOLF4, WOLF5
+    thermal_histories = [WOLF1, WOLF2, WOLF3, WOLF4, WOLF5]
+    for thermal_history in thermal_histories:
+        thermal_history.get_isothermal_intervals(max_temperature_per_step=3.0)
+        assert(np.diff(thermal_history.temperature).max() <= 3.0)
+
 def test_import_sample_datasets():
     from pyFTracks.ressources import Miller, Gleadow
 
@@ -56,6 +112,25 @@ def test_chi2_test():
     Ni = [41, 22, 63, 71, 90, 7, 14, 41, 49, 79, 52, 76, 74, 66, 39, 44, 86, 90, 91, 41]
     chi2 = FT.chi2_test(Ns, Ni)
     assert chi2 == pytest.approx(0.9292, rel=1e-3)
+
+# def test_Dpar_to_rmr0():
+#     from pyFTracks.annealing_model import convert_Dpar_to_rmr0
+#     assert convert_Dpar_to_rmr0(1.0) == pytest.approx(0.84)
+#     assert convert_Dpar_to_rmr0(1.74) == pytest.approx(0.84)
+#     assert convert_Dpar_to_rmr0(5.0) == pytest.approx(0.)
+#     assert convert_Dpar_to_rmr0(2.1) == pytest.approx(0.79962206086744075)
+
+# def test_clapfu_to_rmr0():
+#     from pyFTracks.annealing_model import convert_Cl_pfu_to_rmr0
+#     assert(convert_Cl_pfu_to_rmr0(0.130), 0.)
+#     assert(convert_Cl_pfu_to_rmr0(0.7), 0.30169548259180623)
+#     assert(convert_Cl_pfu_to_rmr0(0.4), 0.6288689335789452)
+
+# def test_ohapfu_to_rmr0():
+#     from pyFTracks.annealing_model import convert_OH_pfu_to_rmr0
+#     assert(convert_OH_pfu_to_rmr0(0.9), 0.)
+#     assert(convert_OH_pfu_to_rmr0(0.7), 0.30169548259180623)
+#     assert(convert_OH_pfu_to_rmr0(0.4), 0.6288689335789452)
 
 #def test_miller_sample():
 #    from pyFTracks.ressources import Miller
