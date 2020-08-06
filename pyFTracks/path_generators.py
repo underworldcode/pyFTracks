c6bd93c069e07dd06074468fc7da48b0fec039d0
Romain Beucher <romain.beucher@anu.edu.au>
2020-08-06 15:14:48 +1000

    Add inversion First pass

--- pyFTracks/path_generators.py
+++ pyFTracks/path_generators.py
@@ -1,4 +1,6 @@
 import numpy as np
+from pyFTracks.thermal_history import ThermalHistory
+import scipy
 
 class MonteCarloPathGenerator(object):
     
@@ -19,6 +21,16 @@ class MonteCarloPathGenerator(object):
         
         self.add_constraint({'time': (0., 0.), 'temperature': (0., 20.)})
         self.TTPaths = None
+        self._annealing_model = None
+        self.goodness_of_fit_values = None
+
+    @property
+    def annealing_model(self):
+        return self._annealing_model
+
+    @annealing_model.setter
+    def annealing_model(self, value):
+        self._annealing_model = value
         
     def add_constraint(self, constraint):
         
@@ -61,13 +73,75 @@ class MonteCarloPathGenerator(object):
         self.TTPaths[:, :, 0] = time * self.fact_time
         self.TTPaths[:, :, 1] = temperature * self.fact_temperature
         return self.TTPaths
-    
+
+    def run(self, measured_lengths, measured_age, measured_age_error):
+
+        if not self.annealing_model:
+            raise ValueError("""Please provide an Annealing Model""")
+
+        self.goodness_of_fit_values = []
+
+        for path in self.TTPaths:
+            time, temperature = path[:, 0], path[:, 1]
+            history = ThermalHistory(time, temperature + 273.15)
+            self.annealing_model.history = history
+            self.annealing_model.calculate_age()
+            self.goodness_of_fit_values.append(self.merit_function(measured_lengths, measured_age, measured_age_error))
+
+        # sort TTPaths
+        self.goodness_of_fit_values = np.array(self.goodness_of_fit_values)
+        self.TTPaths = self.TTPaths[np.argsort(self.goodness_of_fit_values)][::-1]
+        self.goodness_of_fit_values = self.goodness_of_fit_values[np.argsort(self.goodness_of_fit_values)][::-1]
+
+        return
+
+    def merit_function(self, measured_lengths, age=None, age_error=None):
+        # We first evaluated goodness of fit for the track length distribution using 
+        # a Kolmogorov-Smirnov test.
+        # The test relies on 2 parameters:
+        # 1) The maximum separation between 2 cumulative distribution functions which represent the
+        # measured and modelled track length.
+        # 2) The number of measuruments
+
+        # The result of the test is the probability that a set of samples taken randomly
+        # from the known modelled distribution would have a greater maximum separation from it on 
+        # a cdf plot than is observed for the sample distribution being tested.
+
+        # The number of tracks counted is the statistical constrain on how well-defined the fission
+        # track length distribution is, we assume that the model distribution is completely known and
+        # we test the measured distribution against it
+
+        # A K-S probability of 0.05 means that, if N random samples were taken from the distribution
+        # described by the calculation result, where N is the number of FT length actually measured, there would be a 5% chance that
+        # the resulting distribution would have a greater maximum separation from the model on a cdf plot than
+        # is observed between the data and the model.
+        KS_test_lengths = scipy.stats.rv_discrete(values=(self.annealing_model.pdf_axis, self.annealing_model.pdf))
+        KS_test_lengths = scipy.stats.kstest(measured_lengths, KS_test_lengths.cdf)[1]
+        
+        # Now do the age
+        norm = scipy.stats.norm()
+        value = (self.annealing_model.ft_model_age - age) / age_error
+        KS_test_age = 1.0 - scipy.stats.kstest(np.array([value]), norm.cdf)[1]
+        return min(KS_test_age, KS_test_lengths)
+
+
     def plot_paths(self, new=False):
         
         import matplotlib.pyplot as plt
         from matplotlib.collections import LineCollection
         from matplotlib.patches import Rectangle
         from matplotlib.collections import PatchCollection
+        import matplotlib as mpl
+
+        #Create a new colormap
+        cmap = mpl.colors.ListedColormap(["pink", "green", "grey"])
+        bounds = [0., 0.05, 0.5, 1.0]
+        norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+
+        if self.goodness_of_fit_values is not None and not new:
+            colors = cmap(norm(self.goodness_of_fit_values))
+        else:
+            colors = "grey"
 
         if new:
             self.generate_paths()
