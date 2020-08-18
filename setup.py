06f580a7a5438e0c984ff78df3f26bc48c3e1537
Romain Beucher <romain.beucher@anu.edu.au>
2020-08-18 13:48:48 +1000

    Remove python 2 support, add python 3.8

--- setup.py
+++ setup.py
@@ -50,13 +50,12 @@ setup(
 
         'License :: OSI Approved :: MIT License',
 
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     packages=["pyFTracks", "pyFTracks/ressources", "pyFTracks/radialplot"],
     keywords='geology thermochronology fission-tracks',
